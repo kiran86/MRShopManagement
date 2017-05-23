52be915332ed3926c9e1c00f57e26f39e597018e
Kiran <kiran.sankar.das@gmail.com>
2017-05-23 07:16:06 +0530

    Still trying to make out the Sale Register

--- MRShopManagement/frmGenRegister.vb
+++ MRShopManagement/frmGenRegister.vb
@@ -79,16 +79,11 @@ Public Class frmGenRegister
     End Function
 
     Private Sub GenerateRegister(ByVal RegType As Integer, ByVal Category As String)
-        Dim dataAdapter As OleDbDataAdapter
-        Dim dataSet As DataSet = New DataSet
-        Dim dataTable = dataSet.Tables
-        Dim bindingSource As New BindingSource
-        Dim sql As String
-        Dim dataview As DataView
         Dim cmd As OleDbCommand
         Dim dr As OleDbDataReader
+        Dim sql As String
 
-        Dim row As String()
+        Dim row As DataGridViewRow
 
         Dim startDate As Date
         Dim endDate As Date
@@ -97,6 +92,16 @@ Public Class frmGenRegister
         Dim minMemoNo As Integer
         Dim maxMemoNo As Integer
         Dim cashMemoNo As Integer
+
+        Dim riceScale As Double
+        Dim whetScale As Double
+        Dim attaScale As Double
+        Dim sugrScale As Double
+
+        Dim riceUnit As String
+        Dim whetUnit As String
+        Dim attaUnit As String
+        Dim sugrUnit As String
 
         datagridRegister.Rows.Clear()
 
@@ -118,12 +123,11 @@ Public Class frmGenRegister
                 End With
         End Select
 
-        dataSet.Clear()
         Try
             connection.Open()
 
-            sql = "SELECT MIN(Delivery), MAX(Delivery), MIN(CashMemoNo), MAX(CashMemoNo) FROM Delivery WHERE Category = '" + Category + "'"
-            cmd = New OleDbCommand(sql, connection)
+            Sql = "SELECT MIN(Delivery), MAX(Delivery), MIN(CashMemoNo), MAX(CashMemoNo) FROM Delivery WHERE Category = '" + Category + "'"
+            cmd = New OleDbCommand(Sql, connection)
             dr = cmd.ExecuteReader
             dr.Read()
             If dr.HasRows And Not dr.IsDBNull(0) Then
@@ -133,19 +137,44 @@ Public Class frmGenRegister
                 maxMemoNo = dr.GetInt32(3)
             End If
 
+            Sql = "SELECT * FROM Allotment WHERE Category = '" + Category + "'"
+            cmd = New OleDbCommand(Sql, connection)
+            dr = cmd.ExecuteReader
+            If dr.HasRows Then
+                While dr.Read
+                    Select Case Integer.Parse(dr(0).ToString)
+                        Case 1
+                            riceScale = dr.GetDouble(2)
+                            riceUnit = dr.GetString(3)
+                        Case 2
+                            whetScale = dr.GetDouble(2)
+                            whetUnit = dr.GetString(3)
+                        Case 3
+                            attaScale = dr.GetDouble(2)
+                            attaUnit = dr.GetString(3)
+                        Case 4
+                            sugrScale = dr.GetDouble(2)
+                            sugrUnit = dr.GetString(3)
+                    End Select
+                End While
+            End If
+
             delvDate = startDate
             While delvDate.Date <> endDate.AddDays(1).Date
-                sql = "SELECT RCNo, CashMemoNo FROM Delivery WHERE Category = '" + Category + "' AND Delivery = #" + delvDate + "#"
-                cmd = New OleDbCommand(sql, connection)
+                Sql = "SELECT RCNo, CashMemoNo FROM Delivery WHERE Category = '" + Category + "' AND Delivery = #" + delvDate + "#"
+                cmd = New OleDbCommand(Sql, connection)
                 dr = cmd.ExecuteReader
                 If dr.HasRows Then
                     While dr.Read()
-                        row = New String() {delvDate.ToShortDateString, dr(0).ToString, dr(1).ToString, Category, "", "", "", "", ""}
-                        datagridRegister.Rows.Add(row)
+                        'row = New String() {delvDate.ToShortDateString, dr(0).ToString, dr(1).ToString, Category, Format(riceScale, "###0.000"), Format(whetScale, "###0.000"), Format(attaScale, "###0.000"), Format(sugrScale, "###0.000"), Format(riceScale, "###0.000")}
+                        'datagridRegister.Rows.Add(row)
+                        Dim rowID As Integer = datagridRegister.Rows.Add
+                        row = datagridRegister.Rows(rowID)
+
                     End While
                 End If
-                row = New String() {delvDate.ToShortDateString, "Total", "", Category, "", "", "", "", ""}
-                datagridRegister.Rows.Add(row)
+                'row = New String() {delvDate.ToShortDateString, "Total", "", Category, "", "", "", "", ""}
+                'datagridRegister.Rows.Add(row)
                 delvDate = delvDate.AddDays(1)
             End While
 
@@ -158,11 +187,12 @@ Public Class frmGenRegister
 
     End Sub
 
-    Private Function IsCellValueEqual(ByVal row As Integer, ByVal col As Integer) As Boolean
+    Private Function IsCellValueEqual(ByVal row As Integer) As Boolean
         Dim flag As Boolean = False
-        Dim cell1 As DataGridViewCell = datagridRegister.Rows(row).Cells(col)
-        Dim cell2 As DataGridViewCell = datagridRegister.Rows(row - 1).Cells(col)
-        If cell1.Value = vbNull Or cell2.Value = vbNull Then
+        Dim cell1 As DataGridViewCell = datagridRegister.Rows(row).Cells(2)
+        Dim cell2 As DataGridViewCell = datagridRegister.Rows(row - 1).Cells(2)
+
+        If cell1.Value.ToString = String.Empty Or cell2.Value.ToString = String.Empty Then
             Return flag
         End If
         If Integer.Parse(cell1.Value.ToString) = Integer.Parse(cell2.Value.ToString()) Then
@@ -176,7 +206,7 @@ Public Class frmGenRegister
         If e.RowIndex < 1 Or e.ColumnIndex < 0 Then
             Return
         End If
-        If IsCellValueEqual(e.RowIndex, e.ColumnIndex) Then
+        If IsCellValueEqual(e.RowIndex) Then
             e.AdvancedBorderStyle.Top = DataGridViewAdvancedCellBorderStyle.None
         Else
             e.AdvancedBorderStyle.Top = datagridRegister.AdvancedCellBorderStyle.Top
@@ -184,10 +214,10 @@ Public Class frmGenRegister
     End Sub
 
     Private Sub datagridRegister_CellFormatting(sender As Object, e As DataGridViewCellFormattingEventArgs) Handles datagridRegister.CellFormatting
-        If e.RowIndex = 0 Then
+        If e.RowIndex = 0 Or Not e.ColumnIndex = 2 Then
             Return
         End If
-        If IsCellValueEqual(e.RowIndex, e.ColumnIndex) Then
+        If IsCellValueEqual(e.RowIndex) Then
             e.Value = ""
         End If
         e.FormattingApplied = True
