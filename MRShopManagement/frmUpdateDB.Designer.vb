8175e19d56c3e89c119bbadebc0c122afe72af65
Kiran <kiran.sankar.das@gmail.com>
2018-02-24 11:24:36 +0530

    Database update form

---
+++ MRShopManagement/frmUpdateDB.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmUpdateDB
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.lblFile = New System.Windows.Forms.Label()
+        Me.txtbxFileLoc = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "Excel Files(.xls)|*.xls|Excel Files(.xlsx)|*.xlsx|Excel Files(*.xlsm)|*.xlsm"
+        Me.OpenFileDialog1.Title = "Select Database File"
+        '
+        'lblFile
+        '
+        Me.lblFile.AutoSize = True
+        Me.lblFile.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFile.Location = New System.Drawing.Point(12, 9)
+        Me.lblFile.Name = "lblFile"
+        Me.lblFile.Size = New System.Drawing.Size(33, 16)
+        Me.lblFile.TabIndex = 0
+        Me.lblFile.Text = "File:"
+        '
+        'txtbxFileLoc
+        '
+        Me.txtbxFileLoc.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtbxFileLoc.Location = New System.Drawing.Point(51, 6)
+        Me.txtbxFileLoc.Name = "txtbxFileLoc"
+        Me.txtbxFileLoc.ReadOnly = True
+        Me.txtbxFileLoc.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtbxFileLoc.Size = New System.Drawing.Size(322, 22)
+        Me.txtbxFileLoc.TabIndex = 1
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBrowse.Location = New System.Drawing.Point(379, 6)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 2
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Enabled = False
+        Me.btnLoad.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLoad.Location = New System.Drawing.Point(154, 34)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(75, 23)
+        Me.btnLoad.TabIndex = 3
+        Me.btnLoad.Text = "Load"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(235, 34)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Cancel"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'frmUpdateDB
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(465, 60)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtbxFileLoc)
+        Me.Controls.Add(Me.lblFile)
+        Me.Name = "frmUpdateDB"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Update Database"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents lblFile As Label
+    Friend WithEvents txtbxFileLoc As TextBox
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ MRShopManagement/frmUpdateDB.vb
@@ -0,0 +1,51 @@
+﻿Imports DocumentFormat.OpenXml.Packaging
+Imports DocumentFormat.OpenXml.Spreadsheet
+
+Public Class frmUpdateDB
+    Private Sub frmUpdateDB_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        frmMain.Enabled = True
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        OpenFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            txtbxFileLoc.Text = OpenFileDialog1.FileName
+            btnLoad.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        Dim filePath = txtbxFileLoc.Text
+        Try
+            Using doc As SpreadsheetDocument = SpreadsheetDocument.Open(filePath, False)
+                Dim wbPart As WorkbookPart = doc.WorkbookPart
+                Dim sheets = wbPart.Workbook.Descendants(Of Sheet)()
+                Dim newBenfSheet As Worksheet
+                For Each sheet In sheets
+                    If sheet.Name.ToString = "RC Added" Then
+                        newBenfSheet = DirectCast(wbPart.GetPartById(sheet.Id), WorksheetPart).Worksheet
+                        Exit For
+                    End If
+                Next
+                Dim rows As List(Of Row)
+                If newBenfSheet IsNot Nothing Then
+                    Console.WriteLine(newBenfSheet.Count)
+                    Dim sheetData = newBenfSheet.Elements(Of SheetData).First()
+                    rows = sheetData.Elements(Of Row).ToList()
+                End If
+                If rows.Count > 0 Then
+                    Dim row = rows.First
+                    For Each cell As Cell In row.Descendants(Of Cell)
+                        Console.WriteLine(cell.InnerText)
+                    Next
+                End If
+            End Using
+        Catch ex As Exception
+            Console.WriteLine(ex.ToString)
+        End Try
+    End Sub
+End Class
